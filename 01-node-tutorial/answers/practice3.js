f43ce36f6e6d545d1de5829c93bc231a52cd954f
rfswofford <rebecca.swofford42@gmail.com>
2023-01-24 22:18:00 -0500

    week 3

--- 01-node-tutorial/answers/practice3.js
+++ 01-node-tutorial/answers/practice3.js
@@ -3,7 +3,7 @@ fs = require ('fs').promises;
 async function makeFile (){
     try {
         await fs.writeFile('./content/practice2.txt', 'This is the first line.\n')
-        for (let i=2; i<11; i++){
+            for (let i=2; i<11; i++){
         await fs.writeFile('./content/practice2.txt', `This is line ${i} \n`, {flag:'a'})
         }
 
---
+++ 02-express-tutorial/04-express-app.js
@@ -0,0 +1,21 @@
+const express = require('express'); 
+const path = require('path');
+
+const app = express();
+
+//setup static and middleware -- app.use is for setting up middleware
+//static is a resource server doesn't need to change 
+app.use(express.static('./public'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('resource not found')
+})
+
+app.listen(5000, ()=>{
+    console.log('server is listening on port 5000...')
+})
